91bfdc61b14d11d5a08fc4b158cd67c839073453
NikolaevBV <b.nikolaev@sh.dunice.net>
2025-11-27 22:27:29 +0300

    building the app in Program.cs

--- Program.cs
+++ Program.cs
@@ -1,5 +1,12 @@
-var builder = WebApplication.CreateBuilder(args);
+using Todo.NoteProfiles;
+using Todo.Repositories;
+using Todo.Services;
 
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+builder.Services.AddNoteRepository();
+builder.Services.AddNoteServices();
+builder.Services.AddAutoMapper(typeof(NoteProfile));
 
 
 var app = builder.Build();
