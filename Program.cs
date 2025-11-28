7910271d2ca0949198e27cacf61917aaae910f33
NikolaevBV <b.nikolaev@sh.dunice.net>
2025-11-28 23:40:33 +0300

    add swagger

--- Program.cs
+++ Program.cs
@@ -7,10 +7,16 @@ builder.Services.AddControllers();
 builder.Services.AddNoteRepository();
 builder.Services.AddNoteServices();
 builder.Services.AddAutoMapper(typeof(NoteProfile));
+builder.Services.AddSwaggerGen();
 
 
 var app = builder.Build();
 
+if(app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.MapControllers();
 app.Run();
